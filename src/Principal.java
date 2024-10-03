7d540aa82ffe042a2fe2a2bfbab6a23394d60dc9
willson <willsoncatacora1@gmail.com>
2024-10-03 19:37:32 -0300

    se hizo la implementacion de la api como tambien el cliente, resquest y response.

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,10 @@
 //import com.google.gson.Gson;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Principal {
@@ -8,10 +12,23 @@ public class Principal {
         String clave = "e48c2696c3a0e24cad3e2fca";
         Scanner sc = new Scanner(System.in);
         String moneda =  sc.next();
-        String direccion = " https://v6.exchangerate-api.com/v6/" + clave +"/latest/"+moneda;
+        String direccion = "https://v6.exchangerate-api.com/v6/" + clave +"/latest/"+moneda;
         HttpClient cliente = HttpClient.newBuilder().build();
 
         //Gson gson = new Gson();
         System.out.println(direccion);
+        try {
+        //Creando la el cliente HTTP
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest req = HttpRequest.newBuilder().uri(URI.create(direccion)).build();
+        HttpResponse<String> res = cliente.send(req, HttpResponse.BodyHandlers.ofString());
+            System.out.println(res.body());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
