3641f33ad2d5ff3343d22453eebff43d0e70f8e7
willson <willsoncatacora1@gmail.com>
2024-10-03 19:08:38 -0300

    se creo le clave(key) para el uso de la API  y la recepcion para las monedas.

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,17 @@
-import com.google.gson.Gson;
+//import com.google.gson.Gson;
+
+import java.net.http.HttpClient;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-        Gson gson = new Gson();
+        String clave = "e48c2696c3a0e24cad3e2fca";
+        Scanner sc = new Scanner(System.in);
+        String moneda =  sc.next();
+        String direccion = " https://v6.exchangerate-api.com/v6/" + clave +"/latest/"+moneda;
+        HttpClient cliente = HttpClient.newBuilder().build();
+
+        //Gson gson = new Gson();
+        System.out.println(direccion);
     }
 }
